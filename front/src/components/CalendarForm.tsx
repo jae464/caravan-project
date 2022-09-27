c0d383febcecfcd8bd822a3a4b7e46cd9ff1839f
10152954 <minjae12.lee@kt.com>
2022-09-27 16:42:10 +0900

    [FEAT] 컴포넌트 추가 테스트

--- front/src/components/CalendarForm.tsx
+++ front/src/components/CalendarForm.tsx
@@ -1,20 +1,51 @@
 import React, { useState } from 'react'
 import Calendar from 'react-calendar'
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-calendar/dist/Calendar.css';
 import styled from '@emotion/styled';
+import moment from 'moment';
 
 const CalendarForm = () => {
     const [value, onChange] = useState(new Date());
+    const [startDate, setStartDate] = useState(new Date());
+    const onClick = () => {
+        console.log(value);
+    }
   return (
-
-    <CustomCalendar onChange={onChange} />
-
+    <Container>
+        <CustomCalendar onChange={onChange} value={value}/>
+        <DateContainer><span className='date-title'>날짜 : </span>{moment(value).format("YYYY년 MM월 DD일")}</DateContainer>
+        <DatePicker
+            selected={startDate}
+            onChange={(date: Date) => setStartDate(date)}
+            showTimeSelect
+        >시간</DatePicker>
+        {/* <button onClick={onClick}>확인</button> */}
+    </Container>
+    
   )
 }
-
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+`
 const CustomCalendar = styled(Calendar)`
-    width: 100%;
+    width: 80%;
     height: 100%;
     margin: 0 auto;
+    border: none;
+`
+
+const DateContainer = styled.div`
+    margin: 0 auto;
+    margin-top: 2rem;
+    font-size: 1.4rem;
+    .date-title {
+        font-weight: bold;
+    }
+    
 `
 
 
--- front/src/pages/ReservationPage.tsx
+++ front/src/pages/ReservationPage.tsx
@@ -1,15 +1,32 @@
+import React, { useState } from "react";
 import styled from "@emotion/styled";
+
 import CalendarForm from "components/CalendarForm";
 import ReservationForm from "components/ReservationForm";
 import AppLayout from "layouts/AppLayout";
 import ChatBotLayout from "layouts/ChatBotLayout";
 
 const ReservationPage = () => {
+
+  const [count, setCount] = useState(0);
+  const [arr, setArr] = useState<React.ReactNode[]>([]);
+  const onClick = () => {
+    setArr([...arr, <CalendarForm />]);
+    setCount(count + 1);
+    console.log(arr);
+  }
   return (
     <AppLayout>
       <ChatBotLayout>
         <CalendarForm />
       </ChatBotLayout>
+      {arr.map(v=>{
+        return       <ChatBotLayout>
+        {v}
+      </ChatBotLayout>
+      })}
+      <button onClick={onClick}>다음</button>
+
       {/* <ChatBotLayout />
       <ChatBotLayout />
       <ChatBotLayout />
