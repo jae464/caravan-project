abd1b05c8207c6fea5d525b72b405a8ac7c4bbb4
gyeongsu <gyeongsu.kim@kt.com>
2022-10-21 22:16:27 +0900

    [FEAT] 확인 버튼 추가

--- front/src/components/ReservationInfoForm/index.tsx
+++ front/src/components/ReservationInfoForm/index.tsx
@@ -7,6 +7,9 @@ import useComponentHooks from 'hooks/useComponentAdd';
 import ChatBotLayout from 'layouts/ChatBotLayout';
 import CancelForm from 'components/CancelForm';
 import ReservationForm from 'components/ReservationForm';
+import useReservationListHooks from 'hooks/useReservationList';
+import ChatBotText from 'design/ChatBotText';
+import StatusItem from 'components/StatusItem';
 
 type Props = {
     // conferenceInformationData: ConferenceInformationData,
@@ -17,6 +20,7 @@ type Props = {
 
 const ReservationInfoForm = ({reservation}: Props): JSX.Element => {
 
+    const {reservationList, setReservationList, addReservation} = useReservationListHooks([]);
     const {components, setComponent, addComponent} = useComponentHooks([]);
     const [roomName, setRoomName] = useState("");
 
@@ -25,6 +29,36 @@ const ReservationInfoForm = ({reservation}: Props): JSX.Element => {
         addComponent([<ChatBotLayout><ReservationForm info={reservation}></ReservationForm></ChatBotLayout>])
     }
 
+    const onClickConfirm = () => {
+        console.log('confirm')
+        console.log(reservationList.length)
+        if (reservationList.length == 0) {
+            addComponent([
+                <ChatBotLayout>
+                    <ChatBotText>
+                        예약 정보가 없습니다.
+                    </ChatBotText>
+                </ChatBotLayout>
+            ])
+        }
+        else{
+            addComponent([
+                <ChatBotLayout>
+                    <ChatBotText>
+                        예약 상세정보를 확인하고자 할 경우 목록 선택하고, 변경 or 취소를 원하는 경우 체크박스 선택 후 버튼을 눌러주세요. (변경은 1개만 선택가능합니다.)
+                    </ChatBotText>
+                </ChatBotLayout>,
+                <ChatBotLayout>
+                    <ItemContainer>
+                        {reservationList.map( m => {
+                            return (<StatusItem info={m}/>)
+                        })}
+                    </ItemContainer>
+                </ChatBotLayout>
+            ])
+        }
+    }
+ 
     const onClickCancel = () => {
         // alert("삭제")
         addComponent([<ChatBotLayout><CancelForm id={reservation.id!}/></ChatBotLayout>])
@@ -82,6 +116,7 @@ const ReservationInfoForm = ({reservation}: Props): JSX.Element => {
             </Container>
             <ButtonContainer>
                 <ReservationStatusButton onClick={onClickModify}>변경</ReservationStatusButton>
+                <ReservationStatusButton onClick={onClickConfirm}>확인</ReservationStatusButton>
                 <ReservationStatusButton onClick={onClickCancel}>삭제</ReservationStatusButton>
             </ButtonContainer>
         </MainContainer>
@@ -141,6 +176,12 @@ const ReservationStatusButton = styled.button`
     border-radius: 5px;
 `
 
-
+const ItemContainer = styled.div `
+    dispaly: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    // border: 1px solid black;
+`
 
 export default ReservationInfoForm
